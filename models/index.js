8e7d106cce5cb8f26d0dbdf532807a90c783f948
Inna Fedorenko <ivf.fedorenko@gmail.com>
2023-06-19 12:27:42 -0400

    Implemented seeds for all models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
+//  const { Sequelize, DataTypes } = require('sequelize');
+
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
@@ -5,16 +7,17 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category); 
 // Categories have many Products
-
+Product.belongsTo(Category);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: ProductTag }); 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
-  Category,
-  Tag,
-  ProductTag,
+  Category ,
+  Tag  ,
+  ProductTag
 };
